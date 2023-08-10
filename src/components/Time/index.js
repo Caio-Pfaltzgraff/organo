6b2617506340ce915c8bab33879a37d4b66197c2
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-10 15:49:16 -0300

    montando os times e condicional no react

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,13 +1,28 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
 const css = { backgroundColor: props.corSecundaria }
 
+    //if no react
     return (
-        <section className='time' style={css}>
+        (props.colaboradores.length > 0) && <section className='time' style={css}>
             <h3 style={{borderColor : props.corPrimaria}}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+            </div>
         </section>
     )
+
+    // return (
+    //     (props.colaboradores.length > 0) ? <section className='time' style={css}>
+    //         <h3 style={{borderColor : props.corPrimaria}}>{props.nome}</h3>
+    //         <div className='colaboradores'>
+    //             {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+    //         </div>
+    //     </section>
+    //     : ''
+    // )
 }
 
 export default Time
