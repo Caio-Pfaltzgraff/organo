89ab4e8a82f6461650d45a119e5e736403c2fe30
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-10 10:59:17 -0300

    cadastrando colaboradores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 
 function App() {
+
+const [colaboradores, setColaboradores] = useState([]); //array pois virá uma lista
+
+const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]); //cria um novo array, espalha os antigos e coloca o novo no final
+}
+
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,13 +1,17 @@
-import './CampoTexto.css'
+import './CampoTexto.css';
 
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`;
 
+    const aoDigitado = (event) => {
+        props.aoAlterado(event.target.value)
+    }
+
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     );
 }
